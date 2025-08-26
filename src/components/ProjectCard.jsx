4f560f9efe88f35b0a517c32182127cc484bba1d
Olumuyiwa Aro <muyiweraro@gmail.com>
2025-08-26 10:28:22 +0100

    update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,22 @@
-import { Monitor, Github } from "lucide-react"
-import { Tooltip } from "antd"
+import { Monitor, Github } from "lucide-react";
+import { Tooltip } from "antd";
 
 const TechBadge = ({ name }) => {
-  return <span className="px-3 py-1 text-sm font-medium rounded-full bg-[#42446E] text-white">{name}</span>
-}
+  return (
+    <span className="px-3 py-1 text-sm font-medium rounded-full bg-[#42446E] text-white">
+      {name}
+    </span>
+  );
+};
 
-export function ProjectCard({ title, description, imageSrc, technologies, demoUrl, repoUrl }) {
+export function ProjectCard({
+  title,
+  description,
+  imageSrc,
+  technologies,
+  demoUrl,
+  repoUrl,
+}) {
   return (
     <div className=" bg-[#333333] rounded-lg shadow-md overflow-hidden">
       <div className="p-4 h-48 overflow-hidden ">
@@ -19,7 +30,12 @@ export function ProjectCard({ title, description, imageSrc, technologies, demoUr
       </div>
       <div className="p-6">
         <h2 className="text-xl lg:text-2xl font-bold mb-2">{title}</h2>
-        <Tooltip title={description} placement="top" overlayClassName="max-w-xs" mouseEnterDelay={0.5}>
+        <Tooltip
+          title={description}
+          placement="top"
+          overlayClassName="max-w-xs"
+          mouseEnterDelay={0.5}
+        >
           <p className="text-white mb-4 line-clamp-3 cursor-help hover:text-gray-300 transition-colors">
             {description}
           </p>
@@ -32,16 +48,24 @@ export function ProjectCard({ title, description, imageSrc, technologies, demoUr
         </div>
 
         <div className="flex items-center gap-4 text-white">
-          <a href={demoUrl} className="flex items-center gap-1  hover:text-gray-300">
+          <a
+            href={demoUrl}
+            target="_blank"
+            className="flex items-center gap-1  hover:text-gray-300"
+            rel="noreferrer"
+          >
             <Monitor size={18} />
             <span>Demo</span>
           </a>
-          <a href={repoUrl} className="flex items-center gap-1  hover:text-gray-300">
+          <a
+            href={repoUrl}
+            className="flex items-center gap-1  hover:text-gray-300"
+          >
             <Github size={18} />
             <span>Repository</span>
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
